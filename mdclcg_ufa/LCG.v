3fe1caf5f17de274d94c00be40fea1ec6ffd11e1
ajithkreddy <38499006+ajithkreddy@users.noreply.github.com>
2021-05-03 15:58:01 +0530

    Update LCG.v

--- mdclcg_ufa/LCG.v
+++ mdclcg_ufa/LCG.v
@@ -9,7 +9,7 @@ wire [63:0] qi, rqi, sum;
 mux_2x1_64line m1(qi, q, qi1, start);
 Encoder_64x6 En1(r, {a[63:1], 1'b0});
 Barrel_shifter BS1(rqi, qi, r);
-Three_Operand_Binary_Adder adder1(sum, , qi, rqi, b, 1'b0);
+Ultra_Fast_Adder adder1(sum, , qi, rqi, b, 1'b0);
 
 always @(posedge clk)
 qi1 = sum;
